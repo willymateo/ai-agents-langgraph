dea6b23033b01f3433f8b6c65275d28700367909
Willy Joao Mateo Espinoza <67135809+willymateo@users.noreply.github.com>
2025-05-11 17:59:16 -0500

    Add pre-commit settings (#2)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,35 @@
-import streamlit as st
 import os
+
+import streamlit as st
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-document_context_agent_page = st.Page(os.path.join(current_dir,"pages","document_context_agent","agent.py"), title="Document Context Agent", icon="📖", url_path="/document_context_agent")
-api_call_agent_page = st.Page(os.path.join(current_dir,"pages","api_call_agent","agent.py"), title="API Call Agent", icon="🔗",  url_path="/api_call_agent")
-memory_agent_page = st.Page(os.path.join(current_dir,"pages","memory_agent","agent.py"), title="Memory Agent", icon="💾", url_path="/memory_agent")
-main_page = st.Page(os.path.join(current_dir,"pages","home.py"), title="Main Page", icon="🎈", url_path="/")
+document_context_agent_page = st.Page(
+    os.path.join(current_dir, "pages", "document_context_agent", "agent.py"),
+    url_path="/document_context_agent",
+    title="Document Context Agent",
+    icon="📖",
+)
+api_call_agent_page = st.Page(
+    os.path.join(current_dir, "pages", "api_call_agent", "agent.py"),
+    url_path="/api_call_agent",
+    title="API Call Agent",
+    icon="🔗",
+)
+memory_agent_page = st.Page(
+    os.path.join(current_dir, "pages", "memory_agent", "agent.py"),
+    url_path="/memory_agent",
+    title="Memory Agent",
+    icon="💾",
+)
+main_page = st.Page(
+    os.path.join(current_dir, "pages", "home.py"),
+    title="Main Page",
+    url_path="/",
+    icon="🎈",
+)
 
-pg = st.navigation([main_page,document_context_agent_page, api_call_agent_page, memory_agent_page])
+pg = st.navigation(
+    [main_page, document_context_agent_page, api_call_agent_page, memory_agent_page]
+)
 pg.run()
