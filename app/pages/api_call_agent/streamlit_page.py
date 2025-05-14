d01893d8b7181cca763768852e92b744a4a5f49e
willymateo <wjmateo@espol.edu.ec>
2025-05-13 23:16:00 -0500

    Create AI Agent to make API calls

--- app/pages/api_call_agent/streamlit_page.py
+++ app/pages/api_call_agent/streamlit_page.py
@@ -1,3 +1,13 @@
-import streamlit as st
+from app.agents.constants.prompts import GENERAL_PURPOSE_AGENT_SYSTEM_PROMPT
+from app.pages.api_call_agent.graph import graph
+from app.pages.base_ai_agent_chat import BaseAIAgentChat
 
-st.title("AI Agent with API calls")
+api_call_agent = BaseAIAgentChat(
+    title="API Call Agent",
+    name="api_call_agent",
+    graph=graph,
+    description="This agent can call APIs to get information.",
+    system_message=GENERAL_PURPOSE_AGENT_SYSTEM_PROMPT,
+)
+
+api_call_agent.run()
